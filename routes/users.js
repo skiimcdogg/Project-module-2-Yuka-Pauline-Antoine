48453f01ceb7cbd29c18215fefa6640b24178f4f
Yuka TANAKA <seien_0823@icloud.com>
2021-03-09 10:36:36 +0100

    users.js and views

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userModel = require("./../models/User")
 
-
+/* GET user page. */
+router.get('/', function(req, res, next) {
+  res.render('my-page');
+});
 
 module.exports = router;
