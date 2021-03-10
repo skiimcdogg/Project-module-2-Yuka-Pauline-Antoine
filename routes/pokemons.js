484d98ebfa0b94885f6a46ad98eca012fc6c369e
Pauline Bellaud <pauline.bellaud@gmail.com>
2021-03-10 12:06:41 +0100

    pagination test

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -25,6 +25,82 @@ router.get("/pokemons", (req, res, next) => { //name of route to define//
     })
 })
 
+router.get("/pokemons/1", (req, res, next) => { //name of route to define//
+    var interval = {
+        limit: 50,
+        offset: 50
+      }
+    P.getPokemonsList(interval) //see all pokemons//
+    .then((pokemon) => {
+        const arr = pokemon.results.map(p => p.url)
+        P.resource(arr)
+        .then((dbRes) => {
+            // console.log(dbRes)
+            res.render("pokemons/all-pokemons", { pokemon: dbRes });
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+router.get("/pokemons/2", (req, res, next) => { //name of route to define//
+    var interval = {
+        limit: 50,
+        offset: 100
+      }
+    P.getPokemonsList(interval) //see all pokemons//
+    .then((pokemon) => {
+        const arr = pokemon.results.map(p => p.url)
+        P.resource(arr)
+        .then((dbRes) => {
+            // console.log(dbRes)
+            res.render("pokemons/all-pokemons", { pokemon: dbRes });
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+router.get("/pokemons/3", (req, res, next) => { //name of route to define//
+    var interval = {
+        limit: 50,
+        offset: 150
+      }
+    P.getPokemonsList(interval) //see all pokemons//
+    .then((pokemon) => {
+        const arr = pokemon.results.map(p => p.url)
+        P.resource(arr)
+        .then((dbRes) => {
+            // console.log(dbRes)
+            res.render("pokemons/all-pokemons", { pokemon: dbRes });
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+router.get("/pokemons/4", (req, res, next) => { //name of route to define//
+    var interval = {
+        limit: 50,
+        offset: 200
+      }
+    P.getPokemonsList(interval) //see all pokemons//
+    .then((pokemon) => {
+        const arr = pokemon.results.map(p => p.url)
+        P.resource(arr)
+        .then((dbRes) => {
+            // console.log(dbRes)
+            res.render("pokemons/all-pokemons", { pokemon: dbRes });
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
 router.get("/pokemons/:id", (req, res, next) => {
     // console.log(req.params.id)
     P.getPokemonByName(req.params.id) //see one pokemon details//
